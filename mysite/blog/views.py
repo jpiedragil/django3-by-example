e2e611502a91c597a52f18e3e4053592f784d2da
Javier Piedragil <jpiedragil@gmailcom>
2021-01-06 00:26:27 -0600

    📦 NEW: Adding pagination
    - Modified list.html template to include pagination section
    - Add pagination.html template
    - Update views.py to process pagination.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
@@ -6,9 +7,27 @@ from .models import Post
 # Create your views here.
 def post_list(request):
 
-    posts = Post.published.all()
+    object_list = Post.published.all()
 
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    print(paginator)
+    try:
+
+        posts = paginator.page(page)
+
+    except PageNotAnInteger:
+
+        # If page is not an integer deliver the first page.
+        posts = paginator.page(1)
+
+    except EmptyPage:
+
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html',
+                  {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
